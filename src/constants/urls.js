b96acaab0ff7106d3019080b1daf9f58e6b9ec13
Daniil Nazarov <n-danny@yandex.ru>
2017-06-01 16:48:10 +0300

    urls fix

--- src/constants/urls.js
+++ src/constants/urls.js
@@ -1,3 +1,3 @@
-export const MOCK_API_URL = 'http://592b14ba90c9d500119cdc47.mockapi.io/api';
+export const MOCK_API_URL = 'https://592b14ba90c9d500119cdc47.mockapi.io/api';
 export const OPERATIONS_URL = 'https://rocketchatinfo.firebaseio.com/operations.json';
 export const BILLS_URL = 'https://rocketchatinfo.firebaseio.com/bills.json';
